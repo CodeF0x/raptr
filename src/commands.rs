ce8af4f0073eaf72b2a6acc649ed19d57a3057d1
CodeF0x <contact@codef0x.dev>
2020-09-10 09:07:25 +0200

    I finnaly know how to import files :).

--- src/commands.rs
+++ src/commands.rs
@@ -1,3 +1,5 @@
+use crate::web;
+
 #[macro_export]
 macro_rules! vec_of_strings {
     ($($x:expr),*) => (vec![$($x.to_string()),*]);
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 
 mod commands;
+mod web;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
