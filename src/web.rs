a9a0892cf80049c32ada00f31d20ad536d79d556
CodeF0x <contact@codef0x.dev>
2020-09-23 16:07:30 +0200

    working on webinterface, editor and saving markdown

--- src/web.rs
+++ src/web.rs
@@ -2,10 +2,21 @@ use vial;
 
 vial::routes! {
   GET "/" => index;
+  GET "/new" => new;
+  POST "/new" => new;
 }
 
 fn index(req: vial::Request) -> vial::Response {
   vial::Response::from_asset("html/index.html")
+}
+
+fn new(req: vial::Request) -> vial::Response {
+  if req.method() == "GET" {
+
+    vial::Response::from_asset("html/new.html")
+  } else {
+    vial::Response::from_text(req.form("markdown").unwrap())
+  }
 }
 
 pub fn launch_server(port: u16) {
---
+++ static/js/index.js
@@ -0,0 +1,1 @@
+const simplemde = new SimpleMDE();
