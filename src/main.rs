7c8e79fe5a3392e7944699be65befe5ffad48222
CodeF0x <contact@codef0x.dev>
2021-01-11 20:52:07 +0100

    Starting over ¯\_(ツ)_/¯

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,3 @@
-#[macro_use]
-extern crate sailfish_macros;
-
-use std::env;
-
-mod commands;
-mod io;
-mod config;
-
-fn main() -> Result<(), String> {
-    let args: Vec<String> = env::args().skip(1).collect();
-    commands::handle_arguments(args)?;
-    Ok(())
+fn main() {
+    
 }
